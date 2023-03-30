1760699ed7d3670ef526440db57611fba1cbbf5f
sylvahh <sylvesterchambers03@gmail.com>
2023-03-30 02:50:54 +0100

    footer added

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import logo from '../assets/logo~green.png'
+import React from 'react';
+import logo from '../assets/logo~green.png';
 const TopNav = () => {
   return (
-      <header className=' bg-primary sticky top-0 px-5 sm:px-10  py-3 z-40'>
-          <div className="flex justify-between   items-center">
-              <a href="" className='w-full  sm:w-auto'>
-                  
-              <img src={logo} alt="logo" className=' w-[50%] sm:w-auto' />
-              </a>
-              <div className='flex space-x-20 sm:text-xl w-full sm:w-auto'>
-                  <button className='hidden sm:block text-tertiary bg-white py-2 px-5 hover:bg-gray-200 hover:text-secondary rounded-lg'> Add Supermarket</button>
-                  <button className='text-tertiary bg-transparent  hover:text-secondary font-semibold'> My Account</button>
+    <header className='  fixed top-0 left-0 right-0   pb-10 backdrop-blur-[1.5px]  z-40'>
+      <div className='bg-primary px-5 sm:px-10  shadow-lg py-3'>
 
-              </div>
-          </div>
+      <div className='flex justify-between    items-center'>
+        <a href='' className='w-full  sm:w-auto'>
+          <img src={logo} alt='logo' className=' w-[50%] sm:w-auto' />
+        </a>
+        <div className='flex space-x-20 sm:text-xl w-full sm:w-auto'>
+          <button className='hidden sm:block text-tertiary bg-white py-2 px-5 hover:bg-gray-200 hover:text-secondary rounded-lg'>
+            {' '}
+            Add Supermarket
+          </button>
+          <button className='text-tertiary bg-transparent  hover:text-secondary font-semibold'>
+            {' '}
+            My Account
+          </button>
+        </div>
+      </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default React.memo(TopNav)
+export default React.memo(TopNav);
--- src/view/HomePage.tsx
+++ src/view/HomePage.tsx
@@ -7,6 +7,16 @@ import ladyInCart from "../assets/ladyn'cart.png";
 import vectorYellow from '../assets/Vector~yellow.png';
 import vectorGreen from '../assets/Vector~green.png';
 import motherAndSon from '../assets/son-Mom.png';
+import handshake from '../assets/handshake.svg';
+import constructionWorker from '../assets/construction~worker.svg';
+import deviceShop from '../assets/device~shop.svg';
+import reseller from '../assets/reseller.svg';
+import logo from '../assets/logo~green.png';
+import whatsApp from '../assets/WhatsApp.svg';
+import facebook from '../assets/Facebook.svg';
+import twitter from '../assets/Twitter.svg';
+import mastercard from '../assets/mastercard.png';
+import cardPayment from '../assets/card~payment.png';
 
 import Cards from '../components/cards/Cards';
 import Carousel from '../components/carousel/Carousel';
@@ -16,35 +26,37 @@ const HomePage = () => {
     <Fragment>
       <TopNav />
 
-      <div className='flex  justify-between space-x-10 mt-[6rem] px-5 sm:px-10'>
-        <div className='w-full text-[24px] md:text-[35px] lg:text-[55px]'>
-          <h1>
-            Shop from your <span className='text-primary  underline'> favourite supermarket</span>{' '}
-            right here
-          </h1>
-        </div>
+      <section className='mt-[6rem] sm:mt-[10rem]'>
+        <div className='flex  justify-between space-x-10  px-5 sm:px-10'>
+          <div className='w-full text-[24px] md:text-[35px] lg:text-[55px]'>
+            <h1>
+              Shop from your <span className='text-primary  underline'> favourite supermarket</span>{' '}
+              right here
+            </h1>
+          </div>
 
-        <MarketDropDown />
-      </div>
-      <div className='px-5 sm:px-10 my-20 sm:my-[10rem]'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-20'>
-          <Cards title='Supermarkets in Lagos Island' image={blenco} />
-          <Cards title='Supermarkets in Lagos Mainland' image={supermarket} />
+          <MarketDropDown />
         </div>
-        <div className='flex justify-end my-10 sm:pr-10'>
-          {' '}
-          <a
-            href='/'
-            className='text-lg font-semibold text-primary hover:text-secondary  hover:-translate-y-1 transition-all py-5 '
-          >
+        <div className='px-5 sm:px-10 my-20 sm:my-[10rem]'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-20'>
+            <Cards title='Supermarkets in Lagos Island' image={blenco} />
+            <Cards title='Supermarkets in Lagos Mainland' image={supermarket} />
+          </div>
+          <div className='flex justify-end my-10 sm:pr-10'>
             {' '}
-            See all supermarkets &gt;&gt;{' '}
-          </a>
+            <a
+              href='/'
+              className='text-lg font-semibold text-primary hover:text-secondary  hover:-translate-y-1 transition-all py-5 '
+            >
+              {' '}
+              See all supermarkets &gt;&gt;{' '}
+            </a>
+          </div>
         </div>
-      </div>
+      </section>
 
       {/* section 2 */}
-      <div className='bg-cards p-5  sm:p-10'>
+      <section className='bg-cards p-5  sm:p-10'>
         <div className='relative flex flex-col  lg:flex-row  items-center justify-between '>
           <div className='text-[24px]  sm:text-[55px] lg:absolute top-0 left-[40%] w-fit'>
             <h1>
@@ -77,9 +89,9 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
       {/* section 3 */}
-      <div className='bg-[#F5F5F5] px-5 sm:px-10 pt-20 pb-10 sm:pt-[10rem]'>
+      <section className='bg-[#F5F5F5] px-5 sm:px-10 pt-20 pb-10 sm:pt-[10rem]'>
         <div className='flex flex-col items-center space-y-10 '>
           <div className='text-[24px] md:text-[35px] lg:text-[55px]'>
             <h1 className='inline font-extrabold '> We deliver</h1>
@@ -99,29 +111,108 @@ const HomePage = () => {
             <img src={vectorGreen} alt='vector' className='w-[50%] sm:w-[20%]  sm:ml-[12rem]' />
           </div>
         </div>
-      </div>
+      </section>
 
       {/* secttion 4 */}
 
-      <div className='bg-cards px-5 sm:p-10'>
-        <div className=' flex flex-col justify-between sm:flex-row items-sta'>
-          <h1 className=' sm:hidden capitalize text-[45px] font-semibold text-black'>
-            shoppers reviews{' '}
-          </h1>
-
-          <div className=' sm:-order-first  text-right sm:w-[60%]  '>
-            <img src={motherAndSon} alt='mother and son' className='sm:w-[75%] mx-auto' />
-          </div>
-          <div className='flex flex-col justify-center  space-y-10 items-center w-full sm:w-[40%] '>
-            <h1 className='hidden sm:block  capitalize text-[45px] font-semibold text-black'>
+      <footer className='bg-cards'>
+        <div className=' p-5 sm:p-10'>
+          <div className=' flex flex-col justify-between sm:flex-row '>
+            <h1 className=' sm:hidden capitalize text-[45px] font-semibold text-black'>
               shoppers reviews{' '}
             </h1>
-            <div className='w-full'>
-              <Carousel />
+
+            <div className=' sm:-order-first  text-right sm:w-[60%]  '>
+              <img src={motherAndSon} alt='mother and son' className='sm:w-[75%] mx-auto' />
+            </div>
+            <div className='flex flex-col justify-center  space-y-10 items-center w-full sm:w-[40%] '>
+              <h1 className='hidden sm:block  capitalize text-[45px] font-semibold text-black'>
+                shoppers reviews{' '}
+              </h1>
+              <div className='w-full'>
+                <Carousel />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
+        <div id='yellow-bg' className=' mt-20 relative'>
+          <div className=' w-full text-center absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-black'>
+            <div className='flex flex-col  justify-center'>
+              <img src={handshake} alt='handshake' className='w-[50%] sm:w-[25%] mx-auto' />
+              <h1 className='text-[24px] md:text-[35px] lg:text-[55px] text-white'>
+                Let’s make Magic together
+              </h1>
+            </div>
+
+            <div className='flex flex-col sm:flex-row justify-between my-[10rem] space-y-10  sm:space-y-0 '>
+              <div className='text-center space-y-5 px-20'>
+                <img src={deviceShop} alt='house' className='w-[40%] mx-auto' />
+                <h4 className='font-bold text-2xl '>Register your Supermarket</h4>
+              </div>
+              <div className='text-center space-y-5 px-20'>
+                <img src={reseller} alt='avatar' className='w-[40%] mx-auto' />
+                <h4 className='font-bold text-2xl '>Become a Delivery partnert</h4>
+              </div>
+              <div className='text-center space-y-5 px-20'>
+                <img src={constructionWorker} alt=' man working ' className='w-[40%] mx-auto' />
+                <h4 className='font-bold text-2xl '>Work with Us</h4>
+              </div>
+            </div>
+
+            {/* footer */}
+<hr  className='border-black-sub mx-10'/>
+            <div className=' flex justify-between  space-y-5 sm:space-y-0 flex-wrap px-10 sm:px-10 pt-10'>
+              <div className='flex flex-col  justify-center space-y-2 items-start '>
+                <div className='pb-5'>
+                <img src={logo} alt='logo' className='w-[45%] sm:w-[75%]' />
+                </div>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>Onboard Retail</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>Business Benefits</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>Become a Partner</li>
+              </div>
+              <div className='flex flex-col  justify-center space-y-2 items-start  sm:pt-14'>
+                <span className='text-2xl font-semibold'>Important Links</span>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>About Us</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>How it works</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>FAQs</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>Podcast & Stories</li>
+                <li className=' list-none text-lg  hover:text-tertiary cursor-pointer'>Contact Us</li>
+              </div>
+
+              <div className='flex flex-col  justify-center space-y-2 items-start sm:pt-14'>
+                <span className='text-2xl font-semibold'>Socials</span>
+                <div className='flex '>
+                  <li className=' list-none text-lg cursor-pointer'>
+                    {' '}
+                    <img src={facebook} alt='facebook' className=' w-[50%]' />
+                  </li>
+                  <li className=' list-none text-lg cursor-pointer'>
+                    {' '}
+                    <img src={whatsApp} alt='whatsApp' className=' w-[50%]' />
+                  </li>
+                  <li className=' list-none text-lg cursor-pointer'>
+                    {' '}
+                    <img src={twitter} alt='twitter' className=' w-[50%]' />
+                  </li>
+                </div>
+                <span className='text-2xl font-semibold'>Secured payments</span>
+
+                <div className="flex justify-between">
+                  <img src={cardPayment} alt="card payment" className='w-[35%]' />
+                  <img src={mastercard} alt="mastercard" className='w-[35%]' />
+
+                </div>
+              </div>
+           
+              <div className='text-center w-full pt-20 sm:pt-20'>
+                <span>All right reserved 2022</span>
+           </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* section 5 */}
     </Fragment>
   );
 };
